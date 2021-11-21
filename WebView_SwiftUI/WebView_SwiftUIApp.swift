ac03aa2dd89b9382e4362f0f7bdeee2787452b95
SeongMin-K <eo1tks3@naver.com>
2021-11-21 14:50:27 +0900

    Initial Commit

---
+++ WebView_SwiftUI/WebView_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebView_SwiftUIApp.swift
+//  WebView_SwiftUI
+//
+//  Created by SeongMinK on 2021/11/21.
+//
+
+import SwiftUI
+
+@main
+struct WebView_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
